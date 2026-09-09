b90b715bcfb35396f4ca93e0e230d81b657ee107
rraaajamani-blip <rraaajamani@gmail.com>
2026-09-09 16:53:47 +0530

    Initialize foundational infrastructure: package.json, tsconfig, app.json

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@state/(.*)$': '<rootDir>/src/state/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'dist',
+  ],
+  testPathIgnorePatterns: [
+    'node_modules',
+    'dist',
+  ],
+};
